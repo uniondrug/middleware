c5d4c29ad46e7bc611035a72e7b393543d1cb447
倪述荣 <xueron@xueron.com>
2018-03-01 13:37:07 +0800

    use framework's injectable

--- src/Middleware.php
+++ src/Middleware.php
@@ -6,10 +6,10 @@
 
 namespace Uniondrug\Middleware;
 
-use Phalcon\Di\Injectable;
 use Phalcon\Http\RequestInterface;
 use Phalcon\Http\Response;
 use Phalcon\Http\ResponseInterface;
+use Uniondrug\Framework\Injectable;
 
 abstract class Middleware extends Injectable implements MiddlewareInterface
 {
