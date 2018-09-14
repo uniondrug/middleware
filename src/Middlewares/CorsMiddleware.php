9ce107b90ab2d9f5c590edbe35cc9ae584a63009
fuyibing <websearch@163.com>
2018-09-14 12:03:05 +0800

    取消跨域
    取消PHP在代码中的跨域设置

--- src/Middlewares/CorsMiddleware.php
+++ src/Middlewares/CorsMiddleware.php
@@ -18,12 +18,16 @@ class CorsMiddleware extends Middleware
         } catch(\Exception $e) {
             $response = $this->serviceServer->withError($e->getMessage(), $e->getCode());
         }
-        if ($response instanceof ResponseInterface) {
-            $response->setHeader('Access-Control-Allow-Origin', $request->getHeader('Origin'));
-            $response->setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, Cookie');
-            $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
-            $response->setHeader('Access-Control-Allow-Credentials', 'true');
-        }
+        // date:2018/09/14
+        // author:付义兵
+        // 跨域设置迁移至NGINX
+        // PHP代码中的设置已经多余, 不需要再处理
+        // if ($response instanceof ResponseInterface) {
+        //    $response->setHeader('Access-Control-Allow-Origin', $request->getHeader('Origin'));
+        //    $response->setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, Cookie');
+        //    $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
+        //    $response->setHeader('Access-Control-Allow-Credentials', 'true');
+        // }
         return $response;
     }
 }
